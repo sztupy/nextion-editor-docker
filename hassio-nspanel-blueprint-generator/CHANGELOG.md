f6a307bbdc2b8b384cab4174e5f860fe8869f69a
Zsolt Sz Sztupák <mail@sztupy.hu>
2025-06-03 11:21:31 +0100

    Add support for non-EU base images

---
+++ hassio-nspanel-blueprint-generator/CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+### Changed
+### Removed
+
+## [0.0.5] - 2025-06-03
+
+### Added
+
+- Added support for non-eu NSPanel configs
+
+[unreleased]: https://github.com/sztupy/nextion-editor-docker/compare/v0.0.5...HEAD
+[0.0.5]: https://github.com/sztupy/nextion-editor-docker/compare/v0.0.4...v0.0.5
--- nextion-hassio/run.sh
+++ nextion-hassio/run.sh
@@ -14,13 +14,41 @@ git clone --depth 1 https://github.com/sztupy/NSPanel_HA_Blueprint
 cd NSPanel_HA_Blueprint/hmi
 
 sudo mkdir -p /app/input
-sudo cp nspanel_eu.HMI /app/input/input.hmi
+
+PICTURE_DIR=eu
+
+case $BASE in
+  eu)
+    sudo cp nspanel_eu.HMI /app/input/input.hmi
+    ;;
+  us)
+    sudo cp nspanel_us.HMI /app/input/input.hmi
+    PICTURE_DIR=us
+    ;;
+  usland)
+    sudo cp nspanel_us_land.HMI /app/input/input.hmi
+    ;;
+  eucjk)
+    sudo cp nspanel_CJK_eu.HMI /app/input/input.hmi
+    ;;
+  uscjk)
+    sudo cp nspanel_CJK_us.HMI /app/input/input.hmi
+    PICTURE_DIR=us
+    ;;
+  uslandcjk)
+    sudo cp nspanel_CJK_us_land.HMI /app/input/input.hmi
+    ;;
+  *)
+    echo "Invalid base"
+    exit 1
+    ;;
+esac
 
 cd dev/ui
 
 sudo cp /haconfig/$BACKGROUND back.png
 ./generate_images.sh back.png
-sudo cp -a eu /app/input/pics
+sudo cp -a $PICTURE_DIR /app/input/pics
 
 ls -la /app/input/pics
 
